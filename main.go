3d064dc6c79821fa5dd9fc412ec1533b1ab3b5c0
Jason Tu <jason_tu@trendmicro.com>
2018-07-29 06:40:36 -0400

    version 3: with real database handle

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"database/sql"
@@ -10,15 +11,22 @@ import (
 
 /*
 var db *sql.DB
+*/
 
-type Auditlog struct {
-	id        int       `json:"id"`
-	log_time  string    `json:"log_time"`
-	admin     string    `json:"admin"`
-	ip        string    `json:"ip"`
-	action    string    `json:"action"`
-	result    string    `json:"result"`
-	detail    string    `json:"detail"`
+type AuditLog struct {
+	Id        int       `json:"id"` 
+	LogTime  int       `json:"log_time" binding:"required"`
+	Admin     string    `json:"admin" binding:"required"`
+	Ip        string    `json:"ip" binding:"required"`
+	Action    string    `json:"action" binding:"required"`
+	Result    string    `json:"result" binding:"required"`
+	Detail    string    `json:"detail"`
+}
+
+/*
+type Login struct {
+	User     string `form:"user" json:"user" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
 }
 */
 
@@ -41,6 +49,40 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "It works")
+	})
+
+	router.POST("/auditlog", func(c *gin.Context) {
+		/*
+        var json Login
+		if err := c.ShouldBindJSON(&json); err == nil {
+			if json.User == "manu" && json.Password == "123" {
+				c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+			} else {
+				c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			}
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		*/
+		var audit AuditLog
+		if err := c.ShouldBindJSON(&audit); err != nil {
+		    fmt.Println("auditlog input error: ", err)
+			c.JSON(http.StatusOK, gin.H{"status": "input data invalid"})
+		}
+		fmt.Println("auditlog log_time: ", audit.LogTime)
+		fmt.Println("auditlog admin: ", audit.Admin)
+		rs, err := db.Exec(
+			"insert into cloud(log_time, admin, ip, action, result, details) values(?, ?, ?, ?, ?, ?)",
+			audit.LogTime, audit.Admin, audit.Ip, audit.Action, audit.Result, "")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		id, err := rs.LastInsertId()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("insert auditlog id {}", id)
+		c.JSON(http.StatusOK, gin.H{"msg": "create auditlog ok"})
 	})
 
 	router.Run(":8000")
